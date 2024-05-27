82939c659a2ce5138c90c4cb29cc2708a83a2ed8
Ekoramadhan13 <enugraha411@gmail.com>
2024-05-27 10:35:40 +0700

    menambahkan class abstraksiklas

---
+++ abstraksi/abstraksi.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class abstraksiklas {
+private: string x, y;
+
+public:
+	void setxy(string a, string b) {
+		x = a;
+		y = b;
+	}
+	void display() {
+		cout << "x = " << x << endl;
+		cout << "y = " << y << endl;
+	}
+};
+int main()
+{
+   
+}
+
